9a1380c835c865581d768cae47acc6ccf2975669
Craig Cravath <craigcravath@gmail.com>
2018-05-21 13:16:46 -0400

    add comment

--- src/main/java/RunnerClass.java
+++ src/main/java/RunnerClass.java
@@ -1,10 +1,12 @@
 
 public class RunnerClass {
 	public static void main(String ... args) {
+		//inheritance
 		PrintSomething something = new PrintSomething();
 		something.printSomethingMethod();
 		PrintSomething somethingElse = new PrintSomethingElse();
 		somethingElse.printSomethingMethod();
+		//composition
 		Printerface print = printerfaceFactory(args.length > 0 ? args[0]:null);
 		print.printSomethingMethod();
 		
